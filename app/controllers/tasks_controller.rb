8bbf3ad5d9250f1002d8b6d1e0dbf091c3366a89
Jonathan Gertig <jcgertig@gmail.com>
2013-09-17 18:03:01 -0500

    all

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,77 @@
+class TasksController < ApplicationController
+  # GET /tasks
+  # GET /tasks.json
+  def index
+    @tasks = Task.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @tasks }
+    end
+  end
+
+  # GET /tasks/new
+  # GET /tasks/new.json
+  def new
+    @task = Task.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @task }
+    end
+  end
+
+  # POST /tasks
+  # POST /tasks.json
+  def create
+    @task = Task.new(params[:task])
+
+    respond_to do |format|
+      if @task.save
+        format.html { create_flash() }
+        format.json { render json: @task, status: :created, location: @task }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /tasks/1
+  # PUT /tasks/1.json
+  def update
+    @task = Task.find(params[:id])
+
+    respond_to do |format|
+      if @task.update_attributes(params[:task])
+        format.html { update_flash()}
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tasks/1
+  # DELETE /tasks/1.json
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.json { head :no_content }
+    end
+  end
+
+  def create_flash
+    flash[:info] = 'Task was successfully created.'
+    redirect_to root_path 
+  end
+
+  def update_flash
+    flash[:success] = 'Task was successfully updated.'
+    redirect_to root_path 
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '28c9a56949570006a0c510f0f9ddab1f441c8171f53fd82791f6280c9666699585dc4b41e022f558edd1f09047c077840aae720eac98648251948f698a89c389'
